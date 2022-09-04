61d535c9c0b83534f87fec85fdd87fb983f60255
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-05 04:08:44 +0530

    Transaction UI Completed

--- lib/screens/transaction/screen_transactions.dart
+++ lib/screens/transaction/screen_transactions.dart
@@ -5,8 +5,30 @@ class ScreenTransactions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Screen Transactions'),
+    return ListView.separated(
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (context, index) {
+        return const Card(
+          elevation: .5,
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 50,
+              child: Text(
+                '12\n DEC',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            title: Text('RS 100000'),
+            subtitle: Text('Travel'),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          height: 5,
+        );
+      },
+      itemCount: 20,
     );
   }
 }
